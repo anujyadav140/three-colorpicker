04613d7731b9249955db843128c8cf8aab9a4b02
Anuj Yadav <anujyadav140@gmail.com>
2025-06-14 13:12:46 -0400

    url changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',       // static‐export mode
+  assetPrefix: './',      // make all JS/CSS/model refs relative
+  trailingSlash: true,    // each page in its own folder (optional)
 };
 
 export default nextConfig;
